83d84b8a121de16ce8636b5125a9d60d5742b6c7
Tatyana Maltseva <t.malcevaa14@gmail.com>
2023-03-21 22:07:11 +0300

    feat: создать функциональность на js

---
+++ script/index.js
@@ -0,0 +1,34 @@
+const editButton = document.querySelector('.profile__button_type_edit');
+const popup = document.querySelector('.popup');
+const closePopupButton = popup.querySelector('.popup__button-close');
+let nameInputPopup = popup.querySelector('.popup__input_type_name');
+let aboutInputPopup = popup.querySelector('.popup__input_type_about');
+let userName = document.querySelector('.profile__username');
+let userAbout = document.querySelector('.profile__about');
+let formElement = popup.querySelector('.popup__container');
+
+function popupOpen() {
+  nameInputPopup.value = userName.textContent;
+  aboutInputPopup.value = userAbout.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  nameInputPopup = nameInputPopup.value;
+  userName.textContent = nameInputPopup;
+  aboutInputPopup = aboutInputPopup.value;
+  userAbout.textContent = aboutInputPopup;
+  popupClose();
+}
+
+
+editButton.addEventListener('click', popupOpen);
+
+closePopupButton.addEventListener('click', popupClose)
+
+formElement.addEventListener('submit', handleFormSubmit);
